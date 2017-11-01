1132e292d5884adc0ab88fa936dd4ea829715fe1
Minho Lee <minho.lee.93@hotmail.com>
2017-11-01 11:12:42 -0400

    fixing up animation and way the page interacts. Currently only works for 1280px b/c of how css is set up.

--- public/js/testing.js
+++ public/js/testing.js
@@ -3,7 +3,7 @@ var $rect1 = $("#rect1"), $rect2 =$("#rect2");
 var rect1_left = 0, rect2_left = 1000;
 var timer1, timer2;
 var addingDotsTimer;
-var loadingTime = 3000;
+var loadingTime = 1000;
 
 $(document).ready(function() {
 	addingDotsTimer = setInterval(function() {
@@ -21,11 +21,7 @@ var changeColor = function() {
 	if (rect1_left + $rect1.width() > rect2_left) {
 		console.log('CRASHED!');
 		clearInterval(timer1);
-		// $rect1.width(300).css({'background-color': 'green'});
-		timer2 = setInterval(function() {
-			slowMerge();
-			completeMerge();
-		}, 60);
+		$("#resolve-button-div").hide().removeClass('hidden').fadeIn(1000);
 	}
 }
 
@@ -49,15 +45,16 @@ var slowMerge = function() {
 var completeMerge = function() {
 	if (Math.abs(rect1_left - rect2_left) < 1) {
 		// The red rect won't merge perfectly ( <1px difference )
-		$rect2.offset({ left : rect2_left - 0.5 });
-		// console.log(rect1_left+ ' / ' + rect2_left);
+		$rect2.offset({ left : rect2_left - 1.05 });
+		console.log($rect1.offset().left + ' / ' + $rect2.offset().left);
 		console.log('MERGED');
 		clearInterval(timer2);
 		clearInterval(addingDotsTimer);
 
 		$("#rect1-info, #rect2-info").animate({ opacity: 0}, 1500).addClass('hidden');
 		$("#afterResult").hide().removeClass('hidden').slideDown(1000);
-		$("#resolve-button-div").slideUp(500);
+		$("#resolve-button-div").slideUp(1);
+		$("#next-page-div").hide().removeClass('hidden').slideDown(1000);
 		$("#section2 h1").text('Problem Resolved');
 	}
 }
@@ -80,17 +77,23 @@ var addDots = function(id) {
 	}
 }
 
-$("#resolve-button-div").on('click', function() {
+$("#resolve-button-div").on('click', 'button', function() {
+	$("#resolve").prop('disabled', true)
+					 .text('In Progress');
 	clearInterval(addingDotsTimer);
-	// Need to fix this. 'CRASHED' needs to be checked before this button is called.
-	timer1 = setInterval(function() {
-		checkPosition();
-		changeColor();
-	}, 500);
+	timer2 = setInterval(function() {
+		slowMerge();
+		completeMerge();
+	}, 60);
 });
 
 // This is to hide the main 'loading' screen after a certain time (loadingTime).
 window.setTimeout(function() {
 	$("#section1").hide(1000);
-	$("#section2, #resolve-button-div").removeClass('hidden');
+	$("#section2").removeClass('hidden');
+	// Once the section is loaded initially, starting timer1 which initiates movements of the two blobs
+	timer1 = setInterval(function() {
+		checkPosition();
+		changeColor();
+	}, 500);
 }, loadingTime);
