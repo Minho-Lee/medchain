70227e6038b0cb3e2c7f274890206fabadcfeefe
Minho Lee <minho.lee.93@hotmail.com>
2017-11-01 14:37:36 -0400

    making minor changes to all views files. Also now rendering specific patient info

--- public/js/patient-info.js
+++ public/js/patient-info.js
@@ -1,38 +1,38 @@
 $(document).ready(function() {
 
-    $.post("http://localhost:3000/api/Patient", {
-            "$class": "org.acme.medchain.Patient",
-            "patientId": doc[0].name,
-            "name": doc[0].name
-        },
-        function(data, status) {
-            console.log("Data: " + JSON.stringify(data) + "\nStatus: " + status);
-        });
+	 $.post("http://localhost:3000/api/Patient", {
+				"$class": "org.acme.medchain.Patient",
+				"patientId": doc[0].name,
+				"name": doc[0].name
+		  },
+		  function(data, status) {
+				console.log("Data: " + JSON.stringify(data) + "\nStatus: " + status);
+		  });
 
-    $('#submitToDoctor').click(function(e) {
-        var diseaseElement = document.getElementById("diseases");
-        var diseaseToAdd = diseaseElement.options[diseaseElement.selectedIndex].value;
+	 $('#submitToDoctor').click(function(e) {
+		  var diseaseElement = document.getElementById("diseases");
+		  var diseaseToAdd = diseaseElement.options[diseaseElement.selectedIndex].value;
 
-        console.log(diseaseToAdd);
+		  console.log(diseaseToAdd);
 
-        $.post("http://localhost:3000/api/Disease", {
-                "$class": "org.acme.medchain.Disease",
-                "diseaseId": diseaseToAdd,
-                "description": diseaseToAdd,
-                "patient": "none"
-            },
-            function(data, status) {
-                console.log("Data: " + JSON.stringify(data) + "\nStatus: " + status);
+		  $.post("http://localhost:3000/api/Disease", {
+					"$class": "org.acme.medchain.Disease",
+					"diseaseId": diseaseToAdd,
+					"description": diseaseToAdd,
+					"patient": "none"
+				},
+				function(data, status) {
+					 console.log("Data: " + JSON.stringify(data) + "\nStatus: " + status);
 
-                $.post("http://localhost:3000/api/AddDiseaseToPatient", {
-                        "$class": "org.acme.medchain.AddDiseaseToPatient",
-                        "disease": diseaseToAdd,
-                        "newPatient": doc[0].name
-                    },
-                    function(data, status) {
-                        console.log("Data: " + JSON.stringify(data) + "\nStatus: " + status);
-                    });
-            });
-    });
+					 $.post("http://localhost:3000/api/AddDiseaseToPatient", {
+								"$class": "org.acme.medchain.AddDiseaseToPatient",
+								"disease": diseaseToAdd,
+								"newPatient": doc[0].name
+						  },
+						  function(data, status) {
+								console.log("Data: " + JSON.stringify(data) + "\nStatus: " + status);
+						  });
+				});
+	 });
 
 });
