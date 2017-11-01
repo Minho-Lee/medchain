212bb2fc4deb379ec1152eb2b262230b8f8e3f3b
Minho Lee <minho.lee.93@hotmail.com>
2017-10-31 22:30:35 -0400

    adding a button to trigger problem resolve

--- public/js/testing.js
+++ public/js/testing.js
@@ -3,6 +3,7 @@ var $rect1 = $("#rect1"), $rect2 =$("#rect2");
 var rect1_left = 0, rect2_left = 1000;
 var timer1, timer2;
 var addingDotsTimer;
+var loadingTime = 3000;
 
 $(document).ready(function() {
 	addingDotsTimer = setInterval(function() {
@@ -34,7 +35,7 @@ var slowMerge = function() {
 	rect1_left += 0.5;
 	rect2_left -= 0.5;
 	if (initialEnter) {
-		$("#section2 h1").text('Fixing').append('<span class="dots"></span>');
+		$("#section2 h1").text('Fixing ').append('<span class="dots"></span>');
 		initialEnter = false;
 		i = 0;
 		addingDotsTimer = setInterval(function() {
@@ -46,12 +47,16 @@ var slowMerge = function() {
 
 var completeMerge = function() {
 	if (Math.abs(rect1_left - rect2_left) < 1) {
-		// The red rect won't merge perfectly ( <1px difference)
+		// The red rect won't merge perfectly ( <1px difference )
 		$rect2.offset({ left : rect2_left - 0.5 });
-		console.log(rect1_left+ ' / ' + rect2_left);
+		// console.log(rect1_left+ ' / ' + rect2_left);
 		console.log('MERGED');
 		clearInterval(timer2);
 		clearInterval(addingDotsTimer);
+
+		$("#rect1-info, #rect2-info").animate({ opacity: 0}, 1500).addClass('hidden');
+		$("#afterResult").hide().removeClass('hidden').slideDown(1000);
+		$("#resolve-button-div").slideUp(500);
 		$("#section2 h1").text('Problem Resolved');
 	}
 }
@@ -74,15 +79,16 @@ var addDots = function(id) {
 	}
 }
 
-
-
-window.setTimeout(function() {
-	$("#section1").hide();
-	$("#section2").attr('style', 'display:block !important');
+$("#resolve-button-div").on('click', function() {
 	clearInterval(addingDotsTimer);
 	timer1 = setInterval(function() {
 		checkPosition();
 		changeColor();
 	}, 500);
-}, 3000);
-// ^ Change time accordingly
+});
+
+// This is to hide the main 'loading' screen after a certain time (loadingTime).
+window.setTimeout(function() {
+	$("#section1").hide(1000);
+	$("#section2, #resolve-button-div").removeClass('hidden');
+}, loadingTime);
