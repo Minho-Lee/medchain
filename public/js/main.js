e9c9489b1dfbb1783b34d67fcc3f466b4db32eab
Minho Lee <minho.lee.93@hotmail.com>
2017-11-02 14:43:51 -0400

    disabling navbars, they are just indication to show which stage a user is at

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,25 @@
-$(document).ready(function() {
-	$('.nav li a').click(function(e) {
-      //var $btn = $('.nav li button');
-      var $parent = $(this).parent();
-      //var $hash = $(this)[0].hash;
-      $('.nav li').removeClass('active');
+// $(document).ready(function() {
+// 	$('.nav li a').click(function(e) {
+//       //var $btn = $('.nav li button');
+//       var $parent = $(this).parent();
+//       //var $hash = $(this)[0].hash;
+//       $('.nav li').removeClass('active');
       
-      // console.log($parent);
-      // console.log($parent.parent());
+//       // console.log($parent);
+//       // console.log($parent.parent());
 
-      if (!$parent.hasClass('active')) {
-         if ($parent.parent().hasClass('dropdown-menu')) {
-            //adding active class to the list which is a direct child of ul navbar
-            $parent.parent().parent().addClass('active');
+//       if (!$parent.hasClass('active')) {
+//          if ($parent.parent().hasClass('dropdown-menu')) {
+//             //adding active class to the list which is a direct child of ul navbar
+//             $parent.parent().parent().addClass('active');
             
-         } else {
-            $parent.addClass('active');
-         }
-      }
-      e.preventDefault();
-   });
-}); //document.ready
+//          } else {
+//             $parent.addClass('active');
+//          }
+//       }
+//       e.preventDefault();
+//    });
+// }); //document.ready
 
 
 
