5be545349205e2afdd51fbaac72a64e4f6398ec0
Minho Lee <minho.lee.93@hotmail.com>
2017-11-02 14:01:43 -0400

    making changes to patient-info.js -> setting timeOut for form submit" "

--- public/js/patient-info.js
+++ public/js/patient-info.js
@@ -4,35 +4,38 @@ $(document).ready(function() {
 				"$class": "org.acme.medchain.Patient",
 				"patientId": doc[0].name,
 				"name": doc[0].name
-		  },
-		  function(data, status) {
-				console.log("Data: " + JSON.stringify(data) + "\nStatus: " + status);
-		  });
+		},
+		function(data, status) {
+			console.log("Data: " + JSON.stringify(data) + "\nStatus: " + status);
+		});
 
-	 $('#submitToDoctor').click(function(e) {
-		  var diseaseElement = document.getElementById("diseases");
-		  var diseaseToAdd = diseaseElement.options[diseaseElement.selectedIndex].value;
+	$('#submitToDoctor').click(function(e) {
+		var diseaseElement = document.getElementById("diseases");
+		var diseaseToAdd = diseaseElement.options[diseaseElement.selectedIndex].value;
 
-		  console.log(diseaseToAdd);
+		console.log(diseaseToAdd);
 
-		  $.post("http://localhost:3000/api/Disease", {
+		$.post("http://localhost:3000/api/Disease", {
 					"$class": "org.acme.medchain.Disease",
 					"diseaseId": diseaseToAdd,
 					"description": diseaseToAdd,
 					"patient": "none"
 				},
 				function(data, status) {
-					 console.log("Data: " + JSON.stringify(data) + "\nStatus: " + status);
+					console.log("Data: " + JSON.stringify(data) + "\nStatus: " + status);
 
-					 $.post("http://localhost:3000/api/AddDiseaseToPatient", {
+					$.post("http://localhost:3000/api/AddDiseaseToPatient", {
 								"$class": "org.acme.medchain.AddDiseaseToPatient",
 								"disease": diseaseToAdd,
 								"newPatient": doc[0].name
-						  },
-						  function(data, status) {
+						   },
+						   function(data, status) {
 								console.log("Data: " + JSON.stringify(data) + "\nStatus: " + status);
-						  });
-				});
-	 });
+						   });
+		});
 
+		setTimeout(function() {
+            form.submit();
+        	}, 3500);
+	});
 });
