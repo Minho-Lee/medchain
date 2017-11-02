ce5af999f28235d87119f16ee11361ca5781c772
Minho Lee <minho.lee.93@hotmail.com>
2017-11-02 13:53:19 -0400

    adding renderjson.js

--- public/js/doctor-main.js
+++ public/js/doctor-main.js
@@ -1,4 +1,7 @@
  $(document).ready(function() {
-
-
+     $.get("http://localhost:3000/api/system/historian", {},
+         function(data, status) {
+             console.log("Data: " + JSON.stringify(data) + "\nStatus: " + status);
+             document.getElementById("results").appendChild(renderjson(data));
+         });
  });
