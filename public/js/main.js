a7e52ee769a43bd5089923a9f42dc997c93d8c9e
Minho Lee <minho.lee.93@hotmail.com>
2017-11-09 11:42:29 -0500

    changing 'medchain' at top left navbar to match color scheme. Changing height to 100vh. Adding animation to wrench beside 'Fix' button in pharma-main

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,14 @@
 $(document).ready(function() {
-	$(".footer a").hover(
+	$(".footer a, .navbar a.navbar-brand").hover(
 		//This is mouseover
 		function() {
 			// console.log($(this));
-			$(this)[0].firstChild.style.color = '#F33259';
-			$(this)[0].lastChild.style.color ='#F6F3F3';
-
+			$(this)[0].children[0].style.color = '#F33259';
+			$(this)[0].children[1].style.color = '#F6F3F3';
 		//This is mouseleave
 		}, function() {
-			$(this)[0].firstChild.style.color = '#F6F3F3';
-			$(this)[0].lastChild.style.color ='#F33259';
+			$(this)[0].children[0].style.color = '#F6F3F3';
+			$(this)[0].children[1].style.color = '#F33259';
 	});
 });
 
